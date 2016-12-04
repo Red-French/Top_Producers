ff77abbbc22304617ad88b08f9e37740e4751185
Red-French <red@redfrench.net>
2016-12-03 21:46:03 -0600

    move routing to its own file

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,40 +1,43 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
-import { RouterModule }   from '@angular/router';
 
 import { AppComponent }        from './app.component';
-import { HeroesComponent }     from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroService }         from './hero.service';
-import { DashboardComponent }  from './dashboard.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
+
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([  // The 'forRoot' method is used because we're providing a configured router at the root of the
-                            // application. The 'forRoot' method gives us the Router service providers and directives
-                            // needed for routing and performs the initial navigation based on the current browser URL.
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      }
-    ])
+    AppRoutingModule
+    // RouterModule.forRoot([  // The 'forRoot' method is used because we're providing a configured router at the root of the
+    //                         // application. The 'forRoot' method gives us the Router service providers and directives
+    //                         // needed for routing and performs the initial navigation based on the current browser URL.
+    //   {
+    //     path: 'heroes',
+    //     component: HeroesComponent
+    //   },
+    //   {
+    //     path: 'dashboard',
+    //     component: DashboardComponent
+    //   },
+    //   {
+    //     path: 'detail/:id',
+    //     component: HeroDetailComponent
+    //   }
+    // ])
   ],
   declarations: [
     AppComponent,
-    HeroesComponent,
+    DashboardComponent,
     HeroDetailComponent,
-    DashboardComponent
+    HeroesComponent
   ],
   providers: [
     HeroService
