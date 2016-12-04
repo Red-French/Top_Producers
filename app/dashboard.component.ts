a4eba79fb6736046cca4124b2320ea37c329b2d5
Red-French <red@redfrench.net>
2016-12-03 19:37:31 -0600

    initial commit

---
+++ app/dashboard.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-dashboard',
+  templateUrl: 'dashboard.component.html'
+})
+
+export class DashboardComponent implements OnInit {
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}  // Inject HeroService in the constructor and hold it in a private 'heroService'  field
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()  // call the service to get heroes
+    .then(heroes => this.heroes = heroes.slice(0, 4));  // return the top 4
+  }
+
+}
